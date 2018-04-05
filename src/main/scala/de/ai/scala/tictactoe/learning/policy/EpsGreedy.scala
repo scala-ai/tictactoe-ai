1a98e662c51776aa6da9fe5e782228611b1b83fc
Simon Keller <simkelle@htwg-konstanz.de>
2018-04-05 12:12:40 +0200

    Implement eps greedy policy

---
+++ src/main/scala/de/ai/scala/tictactoe/learning/policy/EpsGreedy.scala
@@ -0,0 +1,33 @@
+package de.ai.scala.tictactoe.learning.policy
+
+import scala.util.Random
+
+import de.ai.scala.tictactoe.learning.action.ActionSpace
+
+case class EpsGreedy[S, A](
+    epoch: Long,
+    random: Random,
+    minEpsilon: Float,
+    epsilonNbEpoch: Long,
+    actionSpace: ActionSpace[S, A],
+    actionSupplier: ActionSupplier[S, A]
+) extends Policy[S, A] {
+
+  override def nextAction(input: S): A = {
+    val ep = getEpsilon
+    if (random.nextFloat > ep) {
+      // get actual best action
+      actionSupplier.get(input, actionSpace)
+    } else {
+      // get random action
+      val possibleActions = actionSpace.getPossibleActions(input)
+      possibleActions.toVector(random.nextInt(possibleActions.size))
+    }
+  }
+
+  override def incrementStep(): EpsGreedy[S, A] = copy(epoch = epoch + 1)
+
+  override def resetSteps(): EpsGreedy[S, A] = copy(epoch = 0)
+
+  private def getEpsilon: Float = math.min(1f, math.max(minEpsilon, 1f - epoch.toFloat / epsilonNbEpoch))
+}
--- src/main/scala/de/ai/scala/tictactoe/learning/policy/Policy.scala
+++ src/main/scala/de/ai/scala/tictactoe/learning/policy/Policy.scala
@@ -1,12 +1,17 @@
 package de.ai.scala.tictactoe.learning.policy
 
 import de.ai.scala.tictactoe.learning.action.Action
+import de.ai.scala.tictactoe.learning.action.ActionSpace
 import de.ai.scala.tictactoe.learning.state.State
 
 trait Policy[S >: State, A >: Action] {
   def nextAction(state: S): A
 
-  def incrementStep()
+  def incrementStep(): Policy[S, A]
 
-  def resetSteps()
+  def resetSteps(): Policy[S, A]
+}
+
+trait ActionSupplier[S >: State, A >: Action] {
+  def get(state: S, actionSpace: ActionSpace[S, A]): A
 }
