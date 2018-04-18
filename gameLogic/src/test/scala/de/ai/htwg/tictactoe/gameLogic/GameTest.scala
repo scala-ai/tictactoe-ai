1ad4e15aba4c2a53c2afe755265320c9ccac3e03
aullik <nicolaswehrle@gmx.net>
2018-04-18 15:57:24 +0200

    create GameTest

---
+++ gameLogic/src/test/scala/de/ai/htwg/tictactoe/gameLogic/GameTest.scala
@@ -0,0 +1,70 @@
+package de.ai.htwg.tictactoe.gameLogic
+
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration.FiniteDuration
+
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.pattern.gracefulStop
+import akka.util.Timeout
+import de.ai.htwg.tictactoe.gameLogic.controller.GameControllerActor
+import de.ai.htwg.tictactoe.gameLogic.model.Player
+import de.ai.htwg.tictactoe.gameLogic.model.GridPosition
+import de.ai.htwg.tictactoe.gameLogic.model.SelectPosition
+import de.ai.htwg.tictactoe.gameLogic.model.SelectPositionAck
+
+class GameTest extends GameLogicTestSpec[ActorRef] {
+  implicit val duration: FiniteDuration = Duration(2, TimeUnit.SECONDS)
+  implicit val timeout: Timeout = Timeout(duration)
+  implicit val context: ExecutionContextExecutor = system.dispatcher
+
+  override def setupFixture(): Future[ActorRef] = {
+    val controller: ActorRef = system.actorOf(GameControllerActor.props(Player.One))
+    for {
+      _ <- controller ? SelectPosition(Player.One, GridPosition(0, 0))
+      _ <- controller ? SelectPosition(Player.Two, GridPosition(1, 0))
+      _ <- controller ? SelectPosition(Player.One, GridPosition(0, 1))
+      _ <- controller ? SelectPosition(Player.Two, GridPosition(1, 1))
+      _ <- controller ? SelectPosition(Player.One, GridPosition(0, 2))
+      _ <- controller ? SelectPosition(Player.Two, GridPosition(1, 2))
+    } yield {
+      controller
+    }
+  }
+
+
+  override def cleanupFixture(controller: ActorRef): Future[Unit] = {
+    gracefulStop(controller, duration).map(_ => ())
+  }
+
+  "A game where PLayer ONE starts will finish with" - {
+
+    "Player ONE winning" in { controller =>
+      val pos = GridPosition(0, 3)
+      controller.tell(SelectPosition(Player.One, pos), testActor)
+
+      expectMsgPF(duration) {
+        case SelectPositionAck(Player.One, `pos`, field, SelectPositionAck.SelectPositionReturnCode.GameWon) => field.isFinished shouldBe true
+      }
+    }
+
+
+    "Player TWO winning" in { controller =>
+      val pos1 = GridPosition(2, 0)
+      val pos2 = GridPosition(1, 3)
+      controller.ask(SelectPosition(Player.One, pos1)).onComplete { _ =>
+        controller.tell(SelectPosition(Player.Two, pos2), testActor)
+      }
+
+      expectMsgPF(duration) {
+        case SelectPositionAck(Player.Two, `pos2`, field, SelectPositionAck.SelectPositionReturnCode.GameWon) => field.isFinished shouldBe true
+      }
+
+    }
+  }
+
+}
