c0e276cf7fc64c6b1d5c48124c154a01bbd79617
aullik <nicolaswehrle@gmx.net>
2018-04-22 01:54:06 +0200

    rework GameUiStage

--- playerClient/src/main/scala/de/ai/htwg/tictactoe/playerClient/GameUiStage.scala
+++ playerClient/src/main/scala/de/ai/htwg/tictactoe/playerClient/GameUiStage.scala
@@ -1,8 +1,10 @@
 package de.ai.htwg.tictactoe.playerClient
 
 import scala.collection.JavaConverters
+import scala.concurrent.Promise
+import scala.concurrent.Future
 
-import de.ai.htwg.tictactoe.clientConnection.model.Player
+import de.ai.htwg.tictactoe.clientConnection.model.GridPosition
 import scalafx.geometry.Orientation
 import scalafx.scene.Node
 import scalafx.scene.Scene
@@ -16,141 +18,213 @@ import scalafx.scene.shape.Polygon
 import scalafx.scene.shape.Circle
 import scalafx.stage.Stage
 import scalafx.Includes._
+import scalafx.application.Platform
 
 
 object GameUiStage {
   val defaultAppWidth: Double = 500
   val defaultAppHeight: Double = 500
+
+  trait UIElem {
+    def set(): Unit
+    def clear(): Unit
+  }
+
+  def apply(name: String, dimensions: Int, onMouseClicked: GridPosition => Unit): Future[GameUiStage] = {
+    val p = Promise[GameUiStage]()
+    Platform.runLater(p.success(new GameUiStage(name, dimensions, onMouseClicked)))
+
+    p.future
+  }
+
 }
 
-class GameUiStage(name: String, dimensions: Int, startingPlayer: Player) extends Stage {
-  title = name
+class GameUiStage private(name: String, dimensions: Int, onMouseClicked: GridPosition => Unit) {
 
-  scene = new Scene(GameUiStage.defaultAppWidth, GameUiStage.defaultAppHeight) {
-    val anchor = new AnchorPane
-    root = anchor
+  private val anchor = new AnchorPane
 
-    private var current = startingPlayer
-    private def handleMouseClickedEvent(me: MouseEvent): Unit = {
-      val cellWidth = anchor.width.value / dimensions
-      val cellHeight = anchor.height.value / dimensions
+  private val stage = new Stage {
+    title = name
+    scene = new Scene(GameUiStage.defaultAppWidth, GameUiStage.defaultAppHeight) {
+      root = anchor
 
-      val x = math.floor(me.x / cellWidth).toInt
-      val y = math.floor(me.y / cellHeight).toInt
+      onMouseClicked = handleMouseClickedEvent _
 
 
-      current match {
-        case Player.Cross => addX(x, y)
-        case Player.Circle => addO(x, y)
+      for (i <- 0 to dimensions + 1) {
+        val mod = i.toDouble / dimensions.toDouble
+        val line = Separator(Orientation.Vertical)
+        anchor.children += line
+        AnchorPane.setBottomAnchor(line, 0)
+        AnchorPane.setTopAnchor(line, 0)
+        AnchorPane.setLeftAnchor(line, mod * GameUiStage.defaultAppWidth)
+        anchor.width.onChange {
+          AnchorPane.setLeftAnchor(line, mod * anchor.width.value)
+        }
       }
 
-      current = Player.other(current)
-
-    }
-
-    onMouseClicked = handleMouseClickedEvent _
-
-
-    for (i <- 0 to dimensions + 1) {
-      val mod = i.toDouble / dimensions.toDouble
-      val line = Separator(Orientation.Vertical)
-      anchor.children += line
-      AnchorPane.setBottomAnchor(line, 0)
-      AnchorPane.setTopAnchor(line, 0)
-      AnchorPane.setLeftAnchor(line, mod * GameUiStage.defaultAppWidth)
-      anchor.width.onChange {
-        AnchorPane.setLeftAnchor(line, mod * anchor.width.value)
-      }
-    }
-
-    for (i <- 0 to dimensions + 1) {
-      val mod = i.toDouble / dimensions.toDouble
-      val line = Separator(Orientation.Horizontal)
-      anchor.children += line
-      AnchorPane.setLeftAnchor(line, 0)
-      AnchorPane.setRightAnchor(line, 0)
-      AnchorPane.setTopAnchor(line, mod * GameUiStage.defaultAppHeight)
-      anchor.height.onChange {
-        AnchorPane.setTopAnchor(line, mod * anchor.height.value)
-      }
-    }
-
-    def addElem(x: Int, y: Int, elem: Node, offset: Double): Unit = {
-      anchor.children += elem
-
-      def resize: Unit = {
-        val minSize = Math.min(anchor.width.value, anchor.height.value) / dimensions
-        val scale = minSize / 20
-        elem.scaleX = scale
-        elem.scaleY = scale
-
-        AnchorPane.setTopAnchor(elem, anchor.height.value / dimensions * (y + 0.5) - offset)
-        AnchorPane.setLeftAnchor(elem, anchor.width.value / dimensions * (x + 0.5) - offset)
-      }
-      anchor.width.onChange(resize)
-      anchor.height.onChange(resize)
-      resize
-    }
-
-    def addO(x: Int, y: Int): Unit = {
-
-      val circleRadius = 7.0
-
-      val elem = new Circle {
-        stroke = Green
-        radius = circleRadius
-        fill = Transparent
-
-        strokeWidth = 2
+      for (i <- 0 to dimensions + 1) {
+        val mod = i.toDouble / dimensions.toDouble
+        val line = Separator(Orientation.Horizontal)
+        anchor.children += line
+        AnchorPane.setLeftAnchor(line, 0)
+        AnchorPane.setRightAnchor(line, 0)
+        AnchorPane.setTopAnchor(line, mod * GameUiStage.defaultAppHeight)
+        anchor.height.onChange {
+          AnchorPane.setTopAnchor(line, mod * anchor.height.value)
+        }
       }
 
-      addElem(x, y, elem, circleRadius)
+
     }
-
-    def addX(x: Int, y: Int): Unit = {
-      val size = 16
-      val lineWidth = 3
-
-      val elem = new Polygon() {
-        private val step = (size - lineWidth) / 2.0
-        private val start = 0.0
-        private val end = start + size
-        private val startCross = start + step
-        private val endCross = end - step
-        val pointsList = List(
-          // 0
-          start, startCross,
-          // 1
-          start, endCross,
-          // 2
-          startCross, endCross,
-          // 3
-          startCross, end,
-          // 4
-          endCross, end,
-          // 5
-          endCross, endCross,
-          // 6
-          end, endCross,
-          // 7
-          end, startCross,
-          // 8
-          endCross, startCross,
-          // 9
-          endCross, start,
-          // 10
-          startCross, start,
-          // 11
-          startCross, startCross,
-        )
-        points.setAll(JavaConverters.seqAsJavaList(pointsList.map(double2Double)))
-        rotate = 45
-        fill = Red
-      }
-
-      addElem(x, y, elem, size / 2.0)
-    }
-
-
   }
+
+  anchor.width.onChange(resizeAll())
+  anchor.height.onChange(resizeAll())
+  Platform.runLater(resizeAll())
+
+
+  private def handleMouseClickedEvent(me: MouseEvent): Unit = {
+    val cellWidth = anchor.width.value / dimensions
+    val cellHeight = anchor.height.value / dimensions
+
+    val x = math.floor(me.x / cellWidth).toInt
+    val y = math.floor(me.y / cellHeight).toInt
+
+    val pos = GridPosition(x, y)
+    onMouseClicked(pos)
+  }
+
+  private case class UIElemImpl(elem: Node, resize: (Double, Double) => Unit) extends GameUiStage.UIElem {
+    override def set(): Unit = {
+      Platform.runLater {
+        // just to make sure the element is not already added
+        anchor.children -= elem
+        anchor.children += elem
+      }
+    }
+    override def clear(): Unit = {
+      Platform.runLater {
+        anchor.children -= elem
+      }
+    }
+  }
+
+  private def createElem(x: Int, y: Int, elem: Node, offset: Double): UIElemImpl = {
+    def resize(width: Double, height: Double): Unit = {
+      val minSize = Math.min(width, height) / dimensions
+      val scale = minSize / 20
+      elem.scaleX = scale
+      elem.scaleY = scale
+
+      AnchorPane.setTopAnchor(elem, height / dimensions * (y + 0.5) - offset)
+      AnchorPane.setLeftAnchor(elem, width / dimensions * (x + 0.5) - offset)
+    }
+
+    UIElemImpl(elem, resize)
+  }
+
+  private def createCircle(x: Int, y: Int): UIElemImpl = {
+
+    val circleRadius = 7.0
+
+    val elem = new Circle {
+      stroke = Green
+      radius = circleRadius
+      fill = Transparent
+
+      strokeWidth = 2
+    }
+
+    createElem(x, y, elem, circleRadius)
+  }
+
+  private def createCross(x: Int, y: Int): UIElemImpl = {
+    val size = 16
+    val lineWidth = 3
+
+    val elem = new Polygon() {
+      private val step = (size - lineWidth) / 2.0
+      private val start = 0.0
+      private val end = start + size
+      private val startCross = start + step
+      private val endCross = end - step
+      val pointsList = List(
+        // 0
+        start, startCross,
+        // 1
+        start, endCross,
+        // 2
+        startCross, endCross,
+        // 3
+        startCross, end,
+        // 4
+        endCross, end,
+        // 5
+        endCross, endCross,
+        // 6
+        end, endCross,
+        // 7
+        end, startCross,
+        // 8
+        endCross, startCross,
+        // 9
+        endCross, start,
+        // 10
+        startCross, start,
+        // 11
+        startCross, startCross,
+      )
+      points.setAll(JavaConverters.seqAsJavaList(pointsList.map(double2Double)))
+      rotate = 45
+      fill = Red
+    }
+
+    createElem(x, y, elem, size / 2.0)
+  }
+
+  private val (circles, crosses) = {
+    val (circles, crosses) = (for {
+      x <- 0 until dimensions
+      y <- 0 until dimensions
+    } yield {
+      val pos = GridPosition(x, y)
+      (
+        pos -> createCircle(x, y),
+        pos -> createCross(x, y),
+      )
+    }).unzip
+
+    (circles.toMap, crosses.toMap)
+  }
+
+  private def resizeAll(): Unit = {
+    val width = anchor.width.value
+    val height = anchor.height.value
+    circles.foreach { case (_, uiElem) =>
+      uiElem.resize(width, height)
+    }
+    crosses.foreach { case (_, uiElem) =>
+      uiElem.resize(width, height)
+    }
+  }
+
+  def clear(): Unit = {
+    circles.foreach { case (_, uiElem) =>
+      uiElem.clear()
+    }
+    crosses.foreach { case (_, uiElem) =>
+      uiElem.clear()
+    }
+  }
+  def circle(pos: GridPosition): GameUiStage.UIElem = circles(pos)
+  def cross(pos: GridPosition): GameUiStage.UIElem = crosses(pos)
+
+  def show(): Unit = {
+    Platform.runLater(
+      stage.show()
+    )
+  }
+
+
 }
--- playerClient/src/main/scala/de/ai/htwg/tictactoe/playerClient/Main.scala
+++ playerClient/src/main/scala/de/ai/htwg/tictactoe/playerClient/Main.scala
@@ -1,11 +1,17 @@
 package de.ai.htwg.tictactoe.playerClient
 
+import java.util.concurrent.CountDownLatch
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import de.ai.htwg.tictactoe.clientConnection.model.GridPosition
 import scalafx.application.JFXApp
 import scalafx.application.Platform
 import scalafx.application.JFXApp.PrimaryStage
 
 object Main extends App {
 
+  private val appStarted = new CountDownLatch(1)
   private val app = new JFXApp {
 
     Platform.implicitExit = false
@@ -18,10 +24,26 @@ object Main extends App {
 
     Platform.runLater {
       stage.hide()
+      appStarted.countDown()
     }
   }
 
+  val thread = new Thread(() => {
+    app.main(Array())
+  })
+  thread.start()
 
-  app.main(Array())
+  appStarted.await()
+  println("new Stage")
+
+
+  val stage = GameUiStage("ttt", 4, set)
+
+  def set(pos: GridPosition): Unit = {
+    stage.foreach(_.cross(pos).set())
+  }
+
+  println("show")
+  stage.foreach(_.show())
 
 }
