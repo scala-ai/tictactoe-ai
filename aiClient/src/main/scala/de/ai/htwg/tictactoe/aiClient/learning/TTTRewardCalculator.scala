8eeadb9ab0793a509193b878a74201c521245352
Simon Keller <simkelle@htwg-konstanz.de>
2018-05-03 12:29:22 +0200

    higher immediate reward to give high award to fast wins

--- aiClient/src/main/scala/de/ai/htwg/tictactoe/aiClient/learning/TTTRewardCalculator.scala
+++ aiClient/src/main/scala/de/ai/htwg/tictactoe/aiClient/learning/TTTRewardCalculator.scala
@@ -5,9 +5,9 @@ import de.ai.htwg.tictactoe.aiClient.learning.core.reward.RewardCalculator
 case class TTTRewardCalculator() extends RewardCalculator[TTTAction, TTTState, TTTEpochResult] {
   override def getLongTermReward(runResult: TTTEpochResult): Double = runResult match {
     case r if r.win => 100
-    case r if r.undecided => 10
+    case r if r.undecided => 26
     case _ => -100
   }
 
-  override def getImmediateReward(action: TTTAction, state: TTTState): Double = -0.000001
+  override def getImmediateReward(action: TTTAction, state: TTTState): Double = -1
 }
