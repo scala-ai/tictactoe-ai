57572a2b2b2ab0ad18cecf8347767828adc09f3e
aullik <nicolaswehrle@gmx.net>
2018-05-04 17:10:05 +0200

    TTTLearningProcessor should not be a case class

--- aiClient/src/main/scala/de/ai/htwg/tictactoe/aiClient/learning/TTTLearningProcessor.scala
+++ aiClient/src/main/scala/de/ai/htwg/tictactoe/aiClient/learning/TTTLearningProcessor.scala
@@ -9,16 +9,16 @@ import de.ai.htwg.tictactoe.aiClient.learning.core.policy.ExplorationStepConfigu
 import de.ai.htwg.tictactoe.aiClient.learning.core.policy.PolicyConfiguration
 import de.ai.htwg.tictactoe.aiClient.learning.core.transition.TransitionHistoryImpl
 
-case class TTTLearningProcessor(
+class TTTLearningProcessor(
     learning: QLearning[TTTState, TTTAction, TTTEpochResult]
 ) {
 
   def getDecision(state: TTTState): (TTTAction, TTTLearningProcessor) = {
     val (newLearning, action) = learning.getDecision(state)
-    (action, copy(learning = newLearning))
+    (action, new TTTLearningProcessor(learning = newLearning))
   }
 
-  def trainResult(result: TTTEpochResult): TTTLearningProcessor = copy(learning.trainHistory(result))
+  def trainResult(result: TTTEpochResult): TTTLearningProcessor = new TTTLearningProcessor(learning.trainHistory(result))
 }
 
 object TTTLearningProcessor {
