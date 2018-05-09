dec0f82ee3a02a499acf2204185263fc2b3bfebc
aullik <nicolaswehrle@gmx.net>
2018-05-09 13:58:46 +0200

    add TTTWinStrategyBuilder

---
+++ gameLogic/src/main/scala/de/ai/htwg/tictactoe/gameLogic/controller/TTTWinStrategy.scala
@@ -0,0 +1,18 @@
+package de.ai.htwg.tictactoe.gameLogic.controller
+
+import de.ai.htwg.tictactoe.clientConnection.model.GridPosition
+
+case class TTTWinStrategy(list: List[GridPosition], requiredConnected: Int) {
+
+  def check(checker: GridPosition => Boolean): Boolean = {
+
+    def checkRequirement(list: List[GridPosition], connected: Int): Boolean = list match {
+      case Nil => false
+      case head :: tail =>
+        val newConnected = if (checker(head)) connected + 1 else 0
+        if (newConnected == requiredConnected) true else checkRequirement(tail, newConnected)
+    }
+
+    checkRequirement(list, 0)
+  }
+}
---
+++ gameLogic/src/main/scala/de/ai/htwg/tictactoe/gameLogic/controller/TTTWinStrategyBuilder.scala
@@ -0,0 +1,40 @@
+package de.ai.htwg.tictactoe.gameLogic.controller
+
+import scala.collection.mutable.ListBuffer
+
+import de.ai.htwg.tictactoe.clientConnection.model.GridPosition
+
+trait TTTWinStrategyBuilder {
+  def dimensions: Int
+
+  def listAllWinStrategies: List[TTTWinStrategy]
+
+  protected def listAllGridPos: List[GridPosition] = {
+    (for {
+      x <- 0 until dimensions
+      y <- 0 until dimensions
+    } yield {
+      GridPosition(x, y)
+    }).toList
+  }
+
+  protected def checkDimensions(pos: GridPosition): Boolean = {
+    def check(i: Int) = i >= 0 && i < dimensions
+    check(pos.x) && check(pos.y)
+  }
+
+  private def buildAllWinStrategiesForPos(list: List[TTTWinStrategy]): Map[GridPosition, List[TTTWinStrategy]] = {
+    val map = listAllGridPos.map(_ -> ListBuffer[TTTWinStrategy]()).toMap
+    for {
+      ws <- list
+      pos <- ws.list
+    } {
+      map(pos) += ws
+    }
+    map.mapValues(_.result())
+  }
+
+  def allWinStrategyCheckerPerPos: Map[GridPosition, List[TTTWinStrategy]] = buildAllWinStrategiesForPos(listAllWinStrategies)
+
+}
+
