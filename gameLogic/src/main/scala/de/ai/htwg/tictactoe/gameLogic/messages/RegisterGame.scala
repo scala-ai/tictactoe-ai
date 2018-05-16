6d334a3a8b13b65cefd8be76ce841d71ee241e0c
aullik <nicolaswehrle@gmx.net>
2018-05-16 08:02:01 +0200

    remove deprecated code

--- gameLogic/src/main/scala/de/ai/htwg/tictactoe/gameLogic/messages/RegisterGame.scala
+++
@@ -1,6 +0,0 @@
-package de.ai.htwg.tictactoe.gameLogic.messages
-
-import de.ai.htwg.tictactoe.clientConnection.model.Player
-import de.ai.htwg.tictactoe.gameLogic.controller.GameFieldController
-
-case class RegisterGame(player: Player, gameController: GameFieldController, training: Boolean)
