78555a598112e50841cfdc8b204cfbe025e72795
aullik <nicolaswehrle@gmx.net>
2018-05-18 13:45:25 +0200

    shutdown threadpool if there was an exception

--- clientConnection/src/main/scala/de/ai/htwg/tictactoe/clientConnection/util/SingleThreadPlatform.scala
+++ clientConnection/src/main/scala/de/ai/htwg/tictactoe/clientConnection/util/SingleThreadPlatform.scala
@@ -26,22 +26,18 @@ object SingleThreadPlatform {
     val promise = concurrent.Promise[Runnable]()
     val thread = new Thread(() => {
       Await.result(promise.future, Duration(100, "ms")).run()
-      throw new IllegalStateException("PlatformThread closing")
     })
 
-    @volatile var called = false
     val f: ThreadFactory = r => {
-      if (called) {
-        Thread.sleep(10000)
-        throw new IllegalStateException("Factory called more than once.")
-      }
-      called = true
       promise.success(r)
       thread
     }
 
-    val threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable], f)
-    println("threadpool allows corethread timeout = " + threadPoolExecutor.allowsCoreThreadTimeOut())
+    val threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable], f) {
+      override def afterExecute(r: Runnable, t: Throwable): Unit = {
+        if (t != null) shutdown()
+      }
+    }
     val executionContext = ExecutionContext.fromExecutor(threadPoolExecutor)
     new SingleThreadPlatform(thread, executionContext)
   }
