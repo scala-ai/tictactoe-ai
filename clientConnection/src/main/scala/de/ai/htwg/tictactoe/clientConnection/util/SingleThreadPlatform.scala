496fa2bb5c66c8d5770e8d72aa66b1d5fe05a0c3
aullik <nicolaswehrle@gmx.net>
2018-05-18 04:00:53 +0200

    add temporary extra information to debug SingleThreadPlatform problems

--- clientConnection/src/main/scala/de/ai/htwg/tictactoe/clientConnection/util/SingleThreadPlatform.scala
+++ clientConnection/src/main/scala/de/ai/htwg/tictactoe/clientConnection/util/SingleThreadPlatform.scala
@@ -1,7 +1,9 @@
 package de.ai.htwg.tictactoe.clientConnection.util
 
-import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadFactory
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext
@@ -24,14 +26,23 @@ object SingleThreadPlatform {
     val promise = concurrent.Promise[Runnable]()
     val thread = new Thread(() => {
       Await.result(promise.future, Duration(100, "ms")).run()
+      throw new IllegalStateException("PlatformThread closing")
     })
 
+    @volatile var called = false
     val f: ThreadFactory = r => {
+      if (called) {
+        Thread.sleep(10000)
+        throw new IllegalStateException("Factory called more than once.")
+      }
+      called = true
       promise.success(r)
       thread
     }
 
-    val executionContext = ExecutionContext.fromExecutor(Executors.newSingleThreadExecutor(f))
+    val threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue[Runnable], f)
+    println("threadpool allows corethread timeout = " + threadPoolExecutor.allowsCoreThreadTimeOut())
+    val executionContext = ExecutionContext.fromExecutor(threadPoolExecutor)
     new SingleThreadPlatform(thread, executionContext)
   }
 }
